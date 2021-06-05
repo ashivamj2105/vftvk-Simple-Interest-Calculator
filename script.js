db6862d7c4854dda54909e2ec5d6d1798c4125ea
ashivamj2105 <aryamanshivamjha2105@gmail.com>
2021-06-05 12:26:39 +0530

    Final Update

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    //Input values and calculating Simple Interest
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+
+    //Interest Text displayed on calling Compute function
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 }
-        
+
+// Function for validating Principal amount entered is only a positive number
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
+
+// Function for updating Rate
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+        document.getElementById("rate_val").innerText=rateval;
+}
+       
