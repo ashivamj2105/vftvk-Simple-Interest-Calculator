2a40f556540bc863be6f38424258a5b7e9de65fb
ashivamj2105 <54582829+ashivamj2105@users.noreply.github.com>
2021-06-05 12:33:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute() {
     //Input values and calculating Simple Interest
     var principal = parseFloat(document.getElementById("principal").value);
+    if(isNaN(principal)|| principal<1){
+      alert("Enter a positive Number!");
+      return principal;
+   }
     var rate = parseFloat(document.getElementById("rate").value);
     var years = parseInt(document.getElementById("years").value);
     var interest = principal * years * rate / 100;
@@ -11,17 +15,6 @@ function compute() {
         "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
         "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
         "in the year <mark>" + yearInTheFuture + "</mark>";
-}
-
-// Function for validating Principal amount entered is only a positive number
-function validateAmount() {
-    var principal = document.getElementById("principal").value;
-    var biggerThanZero = parseInt(principal) > 0;
-    if (!biggerThanZero) {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
-    }
-
 }
 
 // Function for updating Rate
